1fdbef48d48728668c1335a95df25a667823f182
onurkurtulmus <onurk.dev@outlook.com>
2022-04-14 12:13:21 +0300

    update

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+	public interface ICoverTypeRepository : IRepository<CoverType>
+	{
+		void Update(CoverType obj);
+
+
+	}
+}
+
--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -3,6 +3,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 {
 	public interface IUnitOfWork
 	{
+		ICoverTypeRepository CoverType { get; }
 		ICategoryRepository Category { get; }
 
 		void Save();
